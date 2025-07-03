0b0fcd171a870d868dce0a5b7b9bc484172868f2
Piotr Niewiadomski <piotrniewiadomski314@gmail.com>
2025-07-03 14:11:51 +0200

    initial structure

---
+++ components/uart_hlk-ld2410s/uart_hlk-ld2410s.c
@@ -0,0 +1,8 @@
+/* 
+The default baud rate of the serial port on the sensor is 115200.
+The port has 1 stop bit and no parity bit.
+1.Enter the command mode.
+2.Set parameter commands or obtain parameter commands.
+3.Exit the command mode.
+HLK-LD2410S data communication uses a small-endian format
+*/
--- main/main.c
+++ main/main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/uart.h"
 #include "esp_log.h"
-#include "mqtt_client.h"
 
 
 
