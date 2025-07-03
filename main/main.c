0c851bd3e938b992dcc76f9230d001f489a658d6
Piotr Niewiadomski <piotrniewiadomski314@gmail.com>
2025-07-03 13:01:27 +0200

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+
+
+void app_main(void)
+{
+
+}
