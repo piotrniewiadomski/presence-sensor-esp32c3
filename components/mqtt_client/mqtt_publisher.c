00e74d9b0caadbf0b735b969f57580ea3eccc462
Piotr Niewiadomski <piotrniewiadomski314@gmail.com>
2025-07-07 12:59:05 +0200

    start of the mqtt app

---
+++ components/mqtt_client/mqtt_publisher.c
@@ -0,0 +1,7 @@
+#include "mqtt_publisher.h"
+#include "mqtt_client.h"
+
+void mqtt_init() 
+{
+    
+}
--- components/mqtt_client/mqtt_publisher.h
+++ components/mqtt_client/mqtt_publisher.h
@@ -1,1 +1,14 @@
-#include "mqtt_client.h"
+#ifndef MQTT_PUBLISHER_H
+#define MQTT_PUBLISHER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void mqtt_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MQTT_PUBLISHER_H
