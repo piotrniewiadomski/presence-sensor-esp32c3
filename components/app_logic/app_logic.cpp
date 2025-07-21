194fcfcfc4df0f7b21a7b5e0d7ed8a8eb2e2b11e
piotrniewiadomski <piotrniewiadomski314@gmail.com>
2025-07-21 17:32:24 +0200

    refactor app_logic: streamline presence detection logic and improve initialization sequence

--- components/app_logic/app_logic.cpp
+++ components/app_logic/app_logic.cpp
@@ -1,47 +1,57 @@
 #include "app_logic.hpp"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "esp_sleep.h"
+#include "freertos/FreeRTOS.h"
 #include "driver/gpio.h"
-#include "driver/rtc_io.h"
 #include "esp_log.h"
+#include "mqtt_publisher.h"
+#include "wifi_service.h"
+#include "uart_hlk_ld2410s.h"
 
-#define HOLD_DURATION_MS 5000
+#define PRESENCE_PIN GPIO_NUM_3
+
+RTC_DATA_ATTR static bool first_boot_done = false;
+RTC_DATA_ATTR static bool wait_for_high = false;
 
 static const char *TAG = "app_logic";
 
-static bool presence_state = false;
-static bool last_presence_state = false;
+void app_logic_main(void) {
+    gpio_config_t io_conf = {
+        .pin_bit_mask = 1ULL << PRESENCE_PIN,
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&io_conf);
 
+    while (1)
+    { 
+        bool presence_level = gpio_get_level(PRESENCE_PIN);
 
-
-void app_logic_task(void *arg)
-{
-    wifi_init();
-    presence_sensor_init();
-    mqtt_init();
-
-    while (1) {
-        if (presence_state == true && last_presence_state == false) {
-            ESP_LOGI(TAG, "Presence detected");
-            wifi_ps_mode_none();
-            mqtt_publish_presence(true);
-            last_presence_state = true;
-            wifi_ps_mode_min_modem();
+        if (!first_boot_done) {
+            presence_sensor_init();
+            wifi_init();
+            mqtt_init();
+            while(!mqtt_connected());
+            first_boot_done = true;
+            ESP_LOGI(TAG, "First boot init done");
+        } else {
+            ESP_LOGI(TAG, "Wake up from deep sleep, by presence level: %d", presence_level);
+            wifi_init();
+            mqtt_init();
+            while(!mqtt_connected());
         }
 
-        if (presence_state == false && last_presence_state == true) {
-            wifi_ps_mode_none();
-            mqtt_publish_presence(false);
-            last_presence_state = false;
-            wifi_ps_mode_min_modem();
-        }
-        presence_state = presence_sensor_state();
+        mqtt_publish_presence(presence_level);
         vTaskDelay(pdMS_TO_TICKS(50));
+        wait_for_high = !presence_level;
+
+        ESP_LOGI(TAG, "Setting wakeup to %s", wait_for_high ? "HIGH" : "LOW");
+        esp_deep_sleep_enable_gpio_wakeup((1 << PRESENCE_PIN), wait_for_high ? ESP_GPIO_WAKEUP_GPIO_HIGH : ESP_GPIO_WAKEUP_GPIO_LOW);
+
+        presence_level = gpio_get_level(PRESENCE_PIN);
+        ESP_LOGI(TAG, "GPIO level before sleep: %d", gpio_get_level(PRESENCE_PIN));
+        if (presence_level != wait_for_high) esp_deep_sleep_start();
     }
 }
 
-void app_logic_main()
-{
-    xTaskCreate(app_logic_task, "app_logic_presence_task", 4096, NULL, 5, NULL);
-}
